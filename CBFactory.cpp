943800834657742ee22259d81f7545f34dc81e3e
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-02-23 17:19:12 +0100

    Factory refactoring

--- CBFactory.cpp
+++ CBFactory.cpp
@@ -20,36 +20,33 @@ namespace cb
 	{
 		auto cPhysics = manager.createComponent<CPhysics>(cbGame, world, true, mPosition, Vector2i{1600, 1600});
 		auto cRender = manager.createComponent<CRender>(cbGame, cPhysics->getBody(), "Images/wall.png");
-		auto& entity = manager.createEntity("wall", {cPhysics, cRender});
 
 		Body& body(cPhysics->getBody());
 		body.addGroups({"solid"});
 
-		return entity;
+		return manager.createEntity("wall", {cPhysics, cRender});
 	}
 	Entity& CBFactory::createBox(Vector2i mPosition)
 	{
 		auto cPhysics = manager.createComponent<CPhysics>(cbGame, world, false, mPosition, Vector2i{1600, 1600});
 		auto cRender = manager.createComponent<CRender>(cbGame, cPhysics->getBody(), "Images/box.png");
-		auto& entity = manager.createEntity("box", {cPhysics, cRender});
 
 		Body& body(cPhysics->getBody());
 		body.addGroups({"solid"});
 		body.addGroupsToCheck({"solid"});
 
-		return entity;
+		return manager.createEntity("box", {cPhysics, cRender});
 	}
 	Entity& CBFactory::createPlayer(Vector2i mPosition)
 	{
 		auto cPhysics = manager.createComponent<CPhysics>(cbGame, world, false, mPosition, Vector2i{1600, 1600});
 		auto cPlayer = manager.createComponent<CPlayer>(cbGame, *cPhysics);
 		auto cRender = manager.createComponent<CRender>(cbGame, cPhysics->getBody(), "Images/player.png");
-		auto& entity = manager.createEntity("player", {cPlayer, cPhysics, cRender});
 
 		Body& body(cPhysics->getBody());
 		body.addGroups({"solid"});
 		body.addGroupsToCheck({"solid"});
 
-		return entity;
+		return manager.createEntity("player", {cPlayer, cPhysics, cRender});
 	}
 }
