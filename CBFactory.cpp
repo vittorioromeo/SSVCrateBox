062259e23155eb31a55b02be17834446a482f0be
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-03-07 18:57:23 +0100

    Adapted to latest SSVS/SSES/SSVCS

--- CBFactory.cpp
+++ CBFactory.cpp
@@ -19,67 +19,72 @@ namespace cb
 
 	Entity& CBFactory::createWall(Vector2i mPosition)
 	{
-		auto cPhysics = manager.createComponent<CPhysics>(cbGame, world, true, mPosition, Vector2i{1600, 1600});
-		auto cRender = manager.createComponent<CRender>(cbGame, cPhysics->getBody(), "Images/wall.png");
+		auto& result = manager.createEntity("wall");
+		auto& cPhysics = result.createComponent<CPhysics>(cbGame, world, true, mPosition, Vector2i{1600, 1600});
+		result.createComponent<CRender>(cbGame, cPhysics.getBody(), "Images/wall.png");
 
-		Body& body(cPhysics->getBody());
+		Body& body(cPhysics.getBody());
 		body.addGroups({"solid"});
 
-		return manager.createEntity("wall", {cPhysics, cRender});
+		return result;
 	}
 	Entity& CBFactory::createBox(Vector2i mPosition)
 	{
-		auto cPhysics = manager.createComponent<CPhysics>(cbGame, world, false, mPosition, Vector2i{1600, 1600});
-		auto cRender = manager.createComponent<CRender>(cbGame, cPhysics->getBody(), "Images/box.png");
+		auto& result = manager.createEntity("box");
+		auto& cPhysics = result.createComponent<CPhysics>(cbGame, world, false, mPosition, Vector2i{1600, 1600});
+		result.createComponent<CRender>(cbGame, cPhysics.getBody(), "Images/box.png");
 
-		Body& body(cPhysics->getBody());
+		Body& body(cPhysics.getBody());
 		body.addGroups({"solid"});
 		body.addGroups({"liftable"});
 		body.addGroups({"crushable"});
 		body.addGroupsToCheck({"solid"});
 
-		return manager.createEntity("box", {cPhysics, cRender});
+		return result;
 	}
 	Entity& CBFactory::createPlayer(Vector2i mPosition)
 	{
-		auto cPhysics = manager.createComponent<CPhysics>(cbGame, world, false, mPosition, Vector2i{1600, 1600});
-		auto cPlayer = manager.createComponent<CPlayer>(cbGame, *cPhysics);
-		auto cRender = manager.createComponent<CRender>(cbGame, cPhysics->getBody(), "Images/player.png");
+		auto& result = manager.createEntity("player");
+		auto& cPhysics = result.createComponent<CPhysics>(cbGame, world, false, mPosition, Vector2i{1600, 1600});
+		result.createComponent<CPlayer>(cbGame, cPhysics);
+		result.createComponent<CRender>(cbGame, cPhysics.getBody(), "Images/player.png");
 
-		Body& body(cPhysics->getBody());
+		Body& body(cPhysics.getBody());
 		body.addGroups({"solid"});
 		body.addGroups({"liftable"});
 		body.addGroups({"crushable"});
 		body.addGroupsToCheck({"solid"});
 
-		return manager.createEntity("player", {cPlayer, cPhysics, cRender});
+		return result;
 	}
 	Entity& CBFactory::createLift(Vector2i mPosition, Vector2f mVelocity)
 	{
-		auto cPhysics = manager.createComponent<CPhysics>(cbGame, world, false, mPosition, Vector2i{1600, 1600}, true);
-		auto cLift = manager.createComponent<CLift>(cbGame, cPhysics->getBody(), mVelocity);
-		auto cRender = manager.createComponent<CRender>(cbGame, cPhysics->getBody(), "Images/lift.png");
+		auto& result = manager.createEntity("lift");
+		auto& cPhysics = result.createComponent<CPhysics>(cbGame, world, false, mPosition, Vector2i{1600, 1600}, true);
+		result.createComponent<CLift>(cbGame, cPhysics.getBody(), mVelocity);
+		auto& cRender =result.createComponent<CRender>(cbGame, cPhysics.getBody(), "Images/lift.png");
 
-		Body& body(cPhysics->getBody());
+		Body& body(cPhysics.getBody());
 		body.addGroups({"solid"});
 		body.addGroupsToCheck({"solid"});
 		body.addGroupsNoResolve({"liftable"});
 
-		if(mVelocity.x == 0) mVelocity.y > 0 ? cRender->setRotation(180) : cRender->setRotation(0);
-		else if(mVelocity.y == 0) mVelocity.x > 0 ? cRender->setRotation(90) : cRender->setRotation(270);
+		if(mVelocity.x == 0) mVelocity.y > 0 ? cRender.setRotation(180) : cRender.setRotation(0);
+		else if(mVelocity.y == 0) mVelocity.x > 0 ? cRender.setRotation(90) : cRender.setRotation(270);
 		
-		return manager.createEntity("lift", {cPhysics, cLift, cRender});
+		return result;
 	}
 	Entity& CBFactory::createMetalBox(Vector2i mPosition)
 	{
-		auto cPhysics = manager.createComponent<CPhysics>(cbGame, world, false, mPosition, Vector2i{1600, 1600});
-		auto cRender = manager.createComponent<CRender>(cbGame, cPhysics->getBody(), "Images/metalbox.png");
+		auto& result = manager.createEntity("metalbox");
+		auto& cPhysics = result.createComponent<CPhysics>(cbGame, world, false, mPosition, Vector2i{1600, 1600});
+		result.createComponent<CRender>(cbGame, cPhysics.getBody(), "Images/metalbox.png");
 
-		Body& body(cPhysics->getBody());
+		Body& body(cPhysics.getBody());
 		body.addGroups({"solid"});
 		body.addGroups({"liftable"});
 		body.addGroupsToCheck({"solid"});
 
-		return manager.createEntity("metalbox", {cPhysics, cRender});
+		return result;
 	}
 }
